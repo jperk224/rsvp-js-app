00914ffcdf2df961c876d27db66209fc9c6904bc
Jeff Perkinson <jmperkinson@gmail.com>
2020-06-15 14:48:33 -0400

    Add edit name function to list items.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
-// form submit handler- captures all paths to form submission
-// const references to the form and input box
+// const global references
 const FORM = document.getElementById('registrar');
 const INPUT = FORM.querySelector('input');  // return the first input tag in the form
 
 const UL = document.getElementById('invitedList');  // declared globally so its available in all functions
 
+// Create an LI and return it to be appended to a ul
 function createLI(text) {
     const LI = document.createElement('li');    // create a new li element to add the input to it
-    LI.textContent = text;
-
+    // wrap the text content in a span for easier DOM manipulation on 'edit' button event
+    const SPAN = document.createElement('span');
+    SPAN.textContent = text;
+    LI.appendChild(SPAN);
     // create a new checkbox to be included in the li
     // this is for the user to check when a list member has RSVP'd
     const LABEL = document.createElement('label');
@@ -17,15 +19,19 @@ function createLI(text) {
     CHECKBOX.type = 'checkbox';
     LABEL.appendChild(CHECKBOX);                // join the checkbox with its label
     LI.appendChild(LABEL);                      // join the checkbox and its label to the li we're pushing into the ul
+    // add an edit button
+    const EDIT_BUTTON = document.createElement('button');
+    EDIT_BUTTON.textContent = 'Edit';
+    LI.appendChild(EDIT_BUTTON);
     // add a button to remove the guest form the list
-    const BUTTON = document.createElement('button');
-    BUTTON.textContent = 'Remove';
-    LI.appendChild(BUTTON);
+    const REMOVE_BUTTON = document.createElement('button');
+    REMOVE_BUTTON.textContent = 'Remove';
+    LI.appendChild(REMOVE_BUTTON);
 
     return LI;
 }
 
-
+// form submit handler- captures all paths to form submission
 FORM.addEventListener('submit', function(e) {   // a shorthand arrow funciton could also be used here
     e.preventDefault();                         // cancel the default submit behavior for the form
     // create a list item to hold the form input submission and append it to the list
@@ -54,14 +60,27 @@ UL.addEventListener('change', function(e) {
 });
 
 // delegated removal handler
-// remove the LI when the 'remove button is clicked
-// also leverages event bubbling up to the ul
+// remove the LI when the 'remove' button is clicked
+// leverages event bubbling up to the ul
 UL.addEventListener('click', function(e) {
-    // filter out elements that aren't buttons
     if (e.target.tagName === 'BUTTON') {
-        // remove the parent li
-        const LI = e.target.parentNode;
+        const BUTTON = e.target;
+        const LI = BUTTON.parentNode;
         const UL = LI.parentNode;
-        UL.removeChild(LI);
+        if(BUTTON.textContent === 'Remove') {
+            UL.removeChild(LI);
+        } else if (BUTTON.textContent === 'Edit') {
+            // the name in the span is the first element in the li
+            const SPAN = LI.firstElementChild;
+            const INPUT = document.createElement('input');
+            INPUT.type = 'text';
+            INPUT.value = SPAN.textContent;
+            // add the input element in front of the span and remove the span
+            // so the user can edit the input field
+            LI.insertBefore(INPUT, SPAN);
+            LI.removeChild(SPAN);
+            // change the 'edit' to a 'save' button
+            BUTTON.textContent = 'Save';
+        }
     }
 });
